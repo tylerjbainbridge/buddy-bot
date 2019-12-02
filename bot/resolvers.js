d2005a585ebc09239b7ce574f108042f192ea8a8
Tyler Bainbridge <tyler@tylerbainbridge.com>
2019-12-02 13:39:20 -0500

    Formatting

--- bot/resolvers.js
+++ bot/resolvers.js
@@ -85,12 +85,12 @@ export const resolvers = {
   sandbox: () =>
     "https://codesandbox.io/s/tylerjbainbridgebuddy-bot-1ce1p?fontsize=14&hidenavigation=1&theme=dark",
   cod: () =>
-    `time to play cod!! ${[
+    `time to play cod!!\n${[
       "googleman#4313",
       "Panderbeah#8947",
       "DoughyDevin#4646",
       "Serey#2198",
       "BrandonKarl#8705",
       "Tyler#0532"
-    ].map(id => `<@${id}>`)}`
+    ].map(id => `<@${id}>\n`)}`
 };
