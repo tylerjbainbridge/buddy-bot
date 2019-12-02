7d4f4e17875326259ab46b31bee0d412a8348807
Tyler Bainbridge <tyler@tylerbainbridge.com>
2019-12-02 14:22:34 -0500

    Use discord filter api

--- bot/utils.js
+++ bot/utils.js
@@ -1,1 +1,9 @@
-export const mentionUsernames = users => users.map(({ id }) => `<@!${id}>\n`);
+export const findByUsername = (users, username) =>
+  users.find(user => user.username === username);
+
+export const filterOutBots = users => users.filter(({ bot }) => !bot);
+
+export const mention = user => `<@!${user.id}>`;
+
+export const mentionUsernames = users =>
+  users.map(user => `${mention(user)}\n`);
