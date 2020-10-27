f23c34ea157c090257b461eed899b9a6c9e57234
Tyler Bainbridge <tylerbainbridge@Tyler-MBP-13-2020.local>
2020-10-27 11:10:22 -0400

    more times

--- bot/schedule.js
+++ bot/schedule.js
@@ -13,17 +13,42 @@ const MONDAY_MONKEY_PIC_URL =
 const FRIDAY_MONKEY_PIC_URL =
   'https://media.discordapp.net/attachments/652152922020511757/769239321286672404/image0.png';
 
+const FUNKY_MONKEY_FRIDAY_URL = 'https://i.imgur.com/TUrYck9.jpg';
+
 export const runSchedule = async (client) => {
   const guild = client.guilds.cache.get(OUR_GUILD_ID);
 
   const generalChannel = getGeneralChannel(guild);
 
+  const botChannel = getBotChannel(client);
+
   while (true) {
     const dateStr = moment().tz('America/New_York').format('dddd|h:mm a');
+
+    // botChannel.send('test', new MessageAttachment(FUNKY_MONKEY_FRIDAY_URL));
 
     const [dayOfWeek, timeofDay] = dateStr.split('|');
 
     switch (timeofDay) {
+      case '12:00 am': {
+        switch (dayOfWeek) {
+          case 'Friday': {
+            generalChannel.send(
+              'FONKY MONKY FRIDAY',
+              new MessageAttachment(FUNKY_MONKEY_FRIDAY_URL)
+            );
+
+            return;
+          }
+        }
+      }
+
+      case '9:00 am': {
+        generalChannel.send('gm');
+
+        return;
+      }
+
       case '9:30 am': {
         switch (dayOfWeek) {
           case 'Monday': {
@@ -34,6 +59,7 @@ export const runSchedule = async (client) => {
 
             return;
           }
+
           case 'Friday': {
             generalChannel.send(
               `IT'S FRIDAY! BABOON DANCE!`,
@@ -44,6 +70,17 @@ export const runSchedule = async (client) => {
           }
         }
       }
+
+      // case '5:00 pm': {
+      //   if (WEEKDAYS.includes(dayOfWeek)) {
+      //     generalChannel.send(
+      //       `stop working!! it's 5pm`,
+      //       new MessageAttachment('https://i.imgur.com/46mKI0H.jpg')
+      //     );
+      //   }
+
+      //   return;
+      // }
     }
 
     await sleep(ONE_MINUTE);
